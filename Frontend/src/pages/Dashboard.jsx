e70408a861b8a2557bcce3715789969c140d5e5f
Luiz Carlos Meira Jr <87606711+luiz-meira-jr@users.noreply.github.com>
2026-06-10 09:01:34 -0300

    Criando rotas no AppRoutes e dando conteúdo ao Dashboard

---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+function Dashboard(){
+    return(
+        <h1>Bem vindo ao painel</h1>
+    )
+}
+
+export default Dashboard;
--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx';
 import Cadastro from '../pages/Cadastro.jsx';
+import Login from '../pages/Login.jsx';
+import Dashboard from '../pages/Dashboard.jsx';
 
 function AppRoutes(){
     return(
@@ -9,6 +11,8 @@ function AppRoutes(){
             <Header/>
             <Routes>
                 <Route path='/cadastro' element={ <Cadastro/> }/>
+                <Route path='/login' element={ <Login/> }/>
+                <Route path='/dashboard' element={ <Dashboard/> }/>
             </Routes>
             <Footer/>
         </BrowserRouter>
